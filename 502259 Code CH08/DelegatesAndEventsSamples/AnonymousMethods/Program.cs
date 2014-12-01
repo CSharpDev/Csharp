cb3cd6641eb918aee81df264f97296de4ecdf51e
lianggui1983 <229042735@qq.com>
2014-12-01 22:10:18 +0800

    11

---
+++ 502259 Code CH08/DelegatesAndEventsSamples/AnonymousMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Wrox.ProCSharp.Delegates
+{
+    class Program
+    {
+        static void Main()
+        {
+            string mid = ", middle part,";
+
+            Func<string, string> anonDel = delegate(string param)
+            {
+                param += mid;
+                param += " and this was added to the string.";
+                return param;
+            };
+            Func<string, string> anonDel2 = param =>
+            {
+                param += mid;
+                param += " and this was added to the string.";
+                return param;
+            };
+            Console.WriteLine(anonDel("Start of string"));
+            Console.WriteLine(anonDel2("Start2 of string"));
+            Console.ReadKey();
+        }
+    }
+
+}
